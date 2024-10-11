61ff2e0aede7337aaf3c31d4116f047d2a98e0a1
ripnsfw222 <136057705+ripnsfw222@users.noreply.github.com>
2024-10-11 00:48:10 -0300

    Create analytics.js

---
+++ analytics.js
@@ -0,0 +1,14 @@
+// analytics.js
+(function() {
+    var gtag = function() {
+        window.dataLayer = window.dataLayer || [];
+        dataLayer.push(arguments);
+    };
+    gtag('js', new Date());
+    gtag('config', 'G-4Q5W50413V');
+
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-4Q5W50413V';
+    document.head.appendChild(script);
+})();
